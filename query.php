551e1280ff6f316553555b272e6bbf5d99bdccf1
Ruiming Cao <caoruiming@gmail.com>
2015-10-13 15:08:44 -0700

    Added query output

---
+++ query.php
@@ -0,0 +1,53 @@
+<html>
+<head><title>CS143 Project 1B Demo</title></head>
+<body>
+Type an SQL query in the following box: <p>
+Example: <tt>SELECT * FROM Actor WHERE id=10;</tt><br />
+<p>
+<form action="." method="GET">
+<textarea name="query" cols="60" rows="8"></textarea><br />
+<input type="submit" value="Submit" />
+</form>
+
+<?php
+	$db_connection = mysql_connect("localhost", "cs143", "");
+	mysql_select_db("CS143", $db_connection);
+
+	$query = "SELECT * FROM Movie where id =100";
+	$rs = mysql_query($query, $db_connection);
+
+	print "<table border=1 cellspacing=1 cellpadding=2>";
+
+	$row = mysql_fetch_assoc($rs);
+	print "<tr align=center>";
+
+	foreach($row as $name=>$value){
+		print "<td>$name</td>";
+	}
+	print "</tr>";
+
+	while($row){
+		print "<tr align=center>";
+		$countRow = count($row);
+
+		foreach($row as $value){
+			print "<td>$value</td>";
+		}
+		print "</tr>";
+		$row = mysql_fetch_row($rs);
+	}
+
+	print "</table>";
+
+	mysql_close($db_connection);
+?>
+
+
+</p>
+<p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small>
+</p>
+
+
+</body>
+</html>
+
