4c9439bae6d337c9d76e89704a28dbe41b3a8353
Ruiming Cao <caoruiming@gmail.com>
2015-10-13 16:28:38 -0700

    Updated constraints

--- create.sql
+++ create.sql
@@ -1,18 +1,59 @@
-create table Movie(id int COMMENT 'Movie ID', title varchar(100) COMMENT 'Movie title', year int COMMENT 'Release year', rating varchar(10) COMMENT 'MPAA rating', company varchar(50) COMMENT 'Producation company');
+CREATE TABLE Movie(
+	id int COMMENT 'Movie ID', 
+	title varchar(100) COMMENT 'Movie title', 
+	year int COMMENT 'Release year', 
+	rating varchar(10) COMMENT 'MPAA rating', 
+	company varchar(50) COMMENT 'Producation company',
+	PRIMARY KEY(id) ENGINE=INNODE);--Movie ID must be unique
 
-create table Actor(id int COMMENT 'Actor ID', last varchar(20) COMMENT 'Last name', first varchar(20) COMMENT 'First name', sex varchar(6) COMMENT 'Sex of the actor', dob DATE COMMENT 'Date of birth', dod DATE COMMENT 'Date of death');
+CREATE TABLE Actor(
+	id int COMMENT 'Actor ID', 
+	last varchar(20) COMMENT 'Last name', 
+	first varchar(20) COMMENT 'First name', 
+	sex varchar(6) COMMENT 'Sex of the actor', 
+	dob DATE COMMENT 'Date of birth', 
+	dod DATE COMMENT 'Date of death',
+	PRIMARY KEY(id),--Actor ID must also be unique
+	CHECK (sex='Female' OR sex='Male'),--sex can either be male or female
+	CHECK (dob<dod) ENGINE=INNODE);--Date of death cannot be earlier than date of birth
 
-create table Director(id int COMMENT 'Director ID', last varchar(20) COMMENT 'Last name', first varchar(20) COMMENT 'First name', dob DATE COMMENT 'Date of birth', dod DATE COMMENT 'Date of death');
+CREATE TABLE Director(
+	id int COMMENT 'Director ID', 
+	last varchar(20) COMMENT 'Last name', 
+	first varchar(20) COMMENT 'First name', 
+	dob DATE COMMENT 'Date of birth', 
+	dod DATE COMMENT 'Date of death',
+	PRIMARY KEY(id),--Director ID must be unique
+	CHECK(dob<dod) ENGINE=INNODE);--Date of death cannot be earlier than date of birth
 
-create table MovieGenre(mid int COMMENT 'Movie ID', genre varchar(20) COMMENT 'Movie genre');
+CREATE TABLE MovieGenre(
+	mid int COMMENT 'Movie ID', 
+	genre varchar(20) COMMENT 'Movie genre',
+	FOREIGN KEY (mid) references Movie(id) ENGINE=INNODE);--mid must match a id in Movie table
 
-create table MovieDirector(mid int COMMENT 'Movie ID', did int COMMENT 'Director ID');
+CREATE TABLE MovieDirector(
+	mid int COMMENT 'Movie ID', 
+	did int COMMENT 'Director ID',
+	FOREIGN KEY (mid) references Movie(id), --mid must match a id in Movie table
+	FOREIGN KEY (did) references Director(id) ENGINE=INNODE);--did must match a id in Director table
 
-create table MovieActor(mid int COMMENT 'Movie ID', aid int COMMENT 'Actor ID', role varchar(50) COMMENT 'Actor role in movie');
+CREATE TABLE MovieActor(
+	mid int COMMENT 'Movie ID', 
+	aid int COMMENT 'Actor ID', 
+	role varchar(50) COMMENT 'Actor role in movie',
+	FOREIGN KEY (mid) references Movie(id), --mid must match a id in Movie table
+	FOREIGN KEY (aid) references Actor(id) ENGINE=INNODE); --aid must match a id in Actor table
 
-create table Review(name varchar(20) COMMENT 'Reviewer name', time TIMESTAMP COMMENT 'Review time', mid int COMMENT 'Movie ID', rating int COMMENT 'Review rating', comment varchar(500) COMMENT 'Reviewer comment');
 
-create table MaxPersonID(id int COMMENT 'Max ID assigned to all persons');
+CREATE TABLE Review(
+	name varchar(20) COMMENT 'Reviewer name', 
+	time TIMESTAMP COMMENT 'Review time', 
+	mid int COMMENT 'Movie ID', 
+	rating int COMMENT 'Review rating', 
+	comment varchar(500) COMMENT 'Reviewer comment',
+	FOREIGN KEY (mid) references Movie(id) ENGINE=INNODE);--mid must match a id in Movie table
 
-create table MaxMovieID(id int COMMENT 'Max ID assigned to all movies');
+CREATE TABLE MaxPersonID(id int COMMENT 'Max ID assigned to all persons' ENGINE=INNODE);
+
+CREATE TABLE MaxMovieID(id int COMMENT 'Max ID assigned to all movies' ENGINE=INNODE);
 
