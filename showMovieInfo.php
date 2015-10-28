f2a85224e770924fb97acefa8bace62db8a49180
Ruiming Cao <caoruiming@gmail.com>
2015-10-28 11:24:04 -0700

    Added comments on showMovieInfo.php

--- showMovieInfo.php
+++ showMovieInfo.php
@@ -60,11 +60,27 @@ $db_connection = mysql_connect("localhost", "cs143", "");
 		for($i = 0; $i < $numrows; $i++)
 		{
 			$row = mysql_fetch_assoc($rs);
-			printf("<li> <a href=\"showActorInfo.php?id=%s\"> %s %s </a> acting as '%s' </li>", $row['id'], $row['first'], $row['last'], $row['role']);
+			printf("<li> <a href=\"showActorInfo.php?id=%s\"> %s %s </a> acting as \"%s\" </li>", $row['id'], $row['first'], $row['last'], $row['role']);
 		}
 		echo "</ul>";
 	}
 	
+	echo "<h3> User Review </h3>";
+	$query = sprintf("SELECT COUNT(*), AVG(rating) FROM Review WHERE mid='%s';", mysql_real_escape_string($id, $db_connection));
+	$rs = mysql_query($query, $db_connection);
+	$info = mysql_fetch_row($rs);
+
+	echo "Average Score: ".$info[1]."/5 (5.0 is best) by ".$info[0]." reviews(s).<a href=\"./addComment.php?mid=".$id."\">  Add your review now!!</a><br>";
+	$query = sprintf("SELECT name, time, rating, comment FROM Review WHERE mid='%s';", mysql_real_escape_string($id, $db_connection));
+	$reviews = mysql_query($query, $db_connection);
+	$review = mysql_fetch_row($reviews);
+	while($review){
+		$date = new DateTime($review[1]);
+		echo "<font color=\"Blue\">In ".$date->format('Y-m-d h-m-s').", <font color=\"Red\">".$review[0]."</font> said: I rate this move score <font color=\"Red\">".$review[2]."</font> point(s), here is my comment. </font><br>";
+		echo $review[3]."<br><br>";
+		$review = mysql_fetch_row($reviews);
+	}
+
 	mysql_close($db_connection);
 ?>
 </body>
